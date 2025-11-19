2139035d3b5d3dec299fe3b84aec1428ca75af8a
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-11-19 20:07:38 +0500

    Task5

--- Tyuiu.KadralinovaAT.Sprint5.Task5.V4.Lib/DataService.cs
+++ Tyuiu.KadralinovaAT.Sprint5.Task5.V4.Lib/DataService.cs
@@ -6,7 +6,32 @@ namespace Tyuiu.KadralinovaAT.Sprint5.Task5.V4.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            throw new NotImplementedException();
+            double res1 = 1;
+            double res2 = 1;
+
+            string allText = File.ReadAllText(path);
+
+            string[] numbers = allText.Split(' ');
+
+            foreach (string value in numbers)
+            {
+                string formattedNum = value.Replace('.', ',');
+
+                double num = Convert.ToDouble(formattedNum);
+
+                if (num == Math.Truncate(num))
+                {
+                    if (num > 0)
+                    {
+                        res1 *= num;
+                    }
+                    if (num < 0)
+                    {
+                        res2 *= num;
+                    }
+                }
+            }
+            return Math.Round(res1-res2, 3);
         }
     }
 }
--- Tyuiu.KadralinovaAT.Sprint5.Task5.V4.Test/DataServiceTest.cs
+++ Tyuiu.KadralinovaAT.Sprint5.Task5.V4.Test/DataServiceTest.cs
@@ -8,6 +8,13 @@ namespace Tyuiu.KadralinovaAT.Sprint5.Task5.V4.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string file = "InPutDataFileTask5V4.txt";
+            string directory = @"C:\DataSprint5";
+            string path = Path.Combine(directory, file);
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
